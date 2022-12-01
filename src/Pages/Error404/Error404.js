cc8cd81be3a65b184eba9c7c51b74025390dff92
Romain Aboa <fr.romisto@gmail.com>
2022-12-01 10:27:45 +0100

    creation de la page Error404

---
+++ src/Pages/Error404/Error404.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Error404.css";
+import { useNavigate } from "react-router-dom";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+
+// création page Error 404
+
+export default function Erreur() {
+  const navigate = useNavigate();
+
+  const backHomePage = () => {
+    navigate("/");
+  };
+  return (
+    <div>
+      <Header />
+      <h1 className="Error">404</h1>
+      <p className="Oups">Oups! La page que vous demandez n'existe pas.</p>
+      <p className="Back" onClick={backHomePage}>
+        Retourner sur la page d'acceuil
+      </p>
+      <Footer />
+    </div>
+  );
+}

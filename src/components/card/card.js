65581084e167a0aa8912e2c292d0bebac9ef65d1
Romain Aboa <fr.romisto@gmail.com>
2022-12-14 23:52:54 +0100

    correction dans card.js

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 const Card = (id, titre, cover) => {
     return (
-        <Link to={`/Accommodation/${id}`} key={id}>
+        <a  href={`/Accommodation/${id}`} key={id}>
               <div className="box">
                 <div className="box-content">
                   <p className="Title">{titre}</p>
                 </div>
                 <img className="Cover" src={cover} alt="Location" />
               </div>
-        </Link> 
+        </a>    
     );
 };
 

019475f1d1faa5b09143cb2758acd1cfc52c42ee
Romain Aboa <fr.romisto@gmail.com>
2022-12-14 20:23:10 +0100

    remplacement de a href par Link to

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 const Card = (id, titre, cover) => {
     return (
-        <a  href={`/Accommodation/${id}`} key={id}>
+        <Link to={`/Accommodation/${id}`} key={id}>
               <div className="box">
                 <div className="box-content">
                   <p className="Title">{titre}</p>
                 </div>
                 <img className="Cover" src={cover} alt="Location" />
               </div>
-        </a>    
+        </Link> 
     );
 };
 
